f632ce71957ac27e240609822ba6d912a0121c9b
Nehemiah Dacres <vivacarlie@gmail.com>
2023-07-09 15:02:31 -0500

    added basis of axum app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Json, Router,
+};
+use std::{net::SocketAddr};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("Server started, listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
 }
+
+#[derive(serde::Serialize)]
+struct Message {
+    message: String,
+}
+
+async fn handler() -> Json<Message> {
+    Json(Message {
+        message: String::from("Hello, World!"),
+    })
+}
+
